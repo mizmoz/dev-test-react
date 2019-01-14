e4fafecbd0d91d83544581752278cbfcd70e68c5
Marian Zburlea <marianzburlea@gmail.com>
2019-01-14 00:11:45 +0000

    add edit and delete button

--- src/components/country-list/country-list.component.js
+++ src/components/country-list/country-list.component.js
@@ -1,15 +1,38 @@
 import React from "react";
+import styled from "styled-components";
+import { style } from "../../configs/theme";
+import Button from "../app/Button";
+
+const CountryListStyled = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+`;
 
 const CountryList = ({ countryData }) => {
   return countryData.length ? (
-    <div>
+    <>
       <h2>Country list by population</h2>
-      {countryData.map(({ countryName, population }) => (
-        <div>
-          {countryName} - {population}
-        </div>
-      ))}
-    </div>
+
+      <CountryListStyled>
+        {[
+          <>
+            <div>Country Name</div>
+            <div>Country Population</div>
+            <div>Actions</div>
+          </>,
+          ...countryData.map(({ countryName, population }) => (
+            <>
+              <div>{countryName}</div>
+              <div>{population}</div>
+              <div>
+                <Button label="✎" color="secondary" />
+                <Button label="×" color="quaternary" />
+              </div>
+            </>
+          ))
+        ]}
+      </CountryListStyled>
+    </>
   ) : (
     <div>
       There is no data about the population of any country. Please use the form
