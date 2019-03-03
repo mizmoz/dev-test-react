0d5df6a4f1d61aba4638f214348b212e64c58455
James Weaver <james.weaver83@yahoo.co.uk>
2019-03-03 14:07:29 +0000

    write some pseudo tests for illustrative purposes

---
+++ src/features/Populations/components/PopulationsForm/tests/index.test.jsx
@@ -0,0 +1,18 @@
+/* Pseudo tests for illustrative purposes */
+describe('<PopulationsForm />', () => {
+  it(`renders a form with a title, dropdown select of countries, population input field, submit button, and an empty
+    "countries by population" list with placeholder when the component mounts`, () => {
+    // do stuff
+  });
+  it(`when form is submitted with population for a new country the country gets added to the "countries by population"
+    list and sorted by population in descending order`, () => {
+    // do stuff
+  });
+  it(`when form is submitted with changed population for an existing country the "countries by population" list is
+    updated with the new value and sorted by population in descending order`, () => {
+    // do stuff
+  });
+  it('when delete button is clicked the relevant country is removed from the "countries by population" list', () => {
+    // do stuff
+  });
+});
