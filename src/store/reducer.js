6c36ab4e8a8a10496383a6083ac81826af72003a
Zdenek Hynek <zdenek.hynek@gmail.com>
2019-09-28 14:22:55 +0100

    Reducing functions

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,8 @@
+/*
+  In real world application, we'd probably have a countriesReducer 
+  to take care of all the countries operation
+  and use ImmutableJS or similar to make the state operations bit simpler
+*/
 import {
   UPDATE_COUNTRIES,
   UPDATE_COUNTRY,
@@ -9,14 +14,49 @@ export const getInitialState = () => ({
 });
 
 export const updateCountries = (state, action) => {
+  if (action) {
+    const { countries } = action;
+    return { ...state, countries };
+  }
+
   return state;
 };
 
 export const updateCountry = (state, action) => {
+  if (action) {
+    const { country } = action;
+    const countries = state.countries.map((c) => {
+      //  is the country we're ammending?
+      if (c.code === country.code) {
+        //  clone all the original props first and then patch
+        //  it with with the update object
+        return {
+          ...c,
+          ...country,
+        };
+      }
+
+      //  country hasn't changed
+      return c;
+    });
+
+    return { ...state, countries };   
+  }
+  
   return state;
 };
 
 export const deleteCountry = (state, action) => {
+  if (action) {
+    const { country } = action;
+    const countries = state.countries.filter((c) => {
+      //  is it the country we're removing
+      return c.code !== country.code;
+    });
+
+    return { ...state, countries };   
+  }
+
   return state;
 };
 
--- src/store/reducer_specs.js
+++ src/store/reducer_specs.js
@@ -1,12 +1,174 @@
 import { expect } from 'chai';
-import { getInitialState } from './reducer';
 
-describe('Reducer - ', () => {
-  describe('getInitialState', () => {
+import {
+  getInitialState,
+  updateCountries,
+  updateCountry,
+  deleteCountry,
+} from './reducer';
+import * as actions from './actions';
+import makeStore from './index';
+import countries from '../configs/country';
+
+describe('Country reducer - ', () => {
+  describe('getInitialState()', () => {
     it('should return empty country list', () => {
       const result = getInitialState();
       expect(result.hasOwnProperty('countries')).to.be.true;
       expect(result.countries.length).to.equal(0);
+    });
+  });
+
+  describe('updateCountries()', () => {
+    it('should insert countries into state', () => {
+      let action = { countries: ['a', 'b', 'c'] };
+      let result = updateCountries(getInitialState(), action);
+      expect(result.countries.length).to.equal(3);
+
+      action = { countries: ['d', 'e'] };
+      result = updateCountries(getInitialState(), action);
+      expect(result.countries.length).to.equal(2);
+    });
+
+    it('should keep other state props', () => {
+      const action = { countries: ['a', 'b', 'c'] };
+      const result = updateCountries({ foo: 'bar' }, action);
+      expect(result.foo).to.equal('bar');
+    });
+  });
+
+  describe('updateCountry()', () => {
+    it('should update correct country', () => {
+      let state = getInitialState();
+      
+      //  insert mock data into state
+      const countriesSample = countries.slice(0, 5); 
+      state = updateCountries(state, { countries: countriesSample });
+      expect(state.countries.length).to.equal(5);
+
+      //  modify one of the countries, making shallow copy first
+      let modifiedCountry = {...countriesSample[2]};
+      modifiedCountry.population = 200;
+      let action = { country: modifiedCountry };
+      state = updateCountry(state, action)
+      expect(state.countries[2].population).to.equal(200);
+      expect(state.countries.length).to.equal(5);
+
+      //  make sure the not update props are kept
+      modifiedCountry = {...state.countries[2]};
+      delete modifiedCountry.population;
+      modifiedCountry.foo = 'bar';
+      action = { country: modifiedCountry };
+      state = updateCountry(state, action)
+      expect(state.countries[2].foo).to.equal('bar');
+      expect(state.countries[2].population).to.equal(200);
+      expect(state.countries.length).to.equal(5);
+    });
+  });
+
+  describe('deleteCountry()', () => {
+    it('should delete correct country', () => {
+      let state = getInitialState();
+
+      //  insert mock data into state
+      const countriesSample = countries.slice(0, 5); 
+      state = updateCountries(state, { countries: countriesSample });
+      expect(state.countries.length).to.equal(5);
+
+      let removeCountry = countriesSample[2];
+      let action = { country: removeCountry };
+      state = deleteCountry(state, action);
+      expect(state.countries.length).to.equal(4);
+
+      removeCountry = countriesSample[1];
+      action = { country: removeCountry };
+      state = deleteCountry(state, action);
+      expect(state.countries.length).to.equal(3);
+
+      //  should ignore invalid data
+      removeCountry = {foo: 'bar'};
+      action = { country: removeCountry };
+      state = deleteCountry(state, action);
+      expect(state.countries.length).to.equal(3);
+    });
+  });
+
+  describe('reduce()', () => {
+    let store;
+
+    beforeEach(() => {
+      store = makeStore();
+    });
+
+    it('should handle UPDATE_COUNTRIES', () => {
+      //  insert mock data into state
+      let countriesSample = countries.slice(0, 5); 
+      let action = actions.updateCountries(countriesSample);
+      store.dispatch(action);
+
+      let state = store.getState();
+      expect(state.countries.length).to.equal(5);
+
+      countriesSample = countries.slice(15, 115); 
+      action = actions.updateCountries(countriesSample);
+      store.dispatch(action);
+
+      state = store.getState();
+      expect(state.countries.length).to.equal(100);
+    });
+
+    it('should handle UPDATE_COUNTRY', () => {
+      //  insert mock data into state
+      let countriesSample = countries.slice(0, 5); 
+      let action = actions.updateCountries(countriesSample);
+      store.dispatch(action);
+
+      let state = store.getState();
+      expect(state.countries.length).to.equal(5);
+
+      let modifiedCountry = {...countriesSample[2]};
+      modifiedCountry.population = 200;
+      action = actions.updateCountry(modifiedCountry);
+      store.dispatch(action);
+      state = store.getState();
+      expect(state.countries[2].population).to.equal(200);
+
+      modifiedCountry = {...countriesSample[3]};
+      modifiedCountry.population = 300;
+      action = actions.updateCountry(modifiedCountry);
+      store.dispatch(action);
+      state = store.getState();
+      expect(state.countries[2].population).to.equal(200);
+      expect(state.countries[3].population).to.equal(300);
+    });
+
+    it('should handle DELETE_COUNTRY', () => {
+      //  insert mock data into state
+      let countriesSample = countries.slice(0, 5); 
+      let action = actions.updateCountries(countriesSample);
+      store.dispatch(action);
+
+      let state = store.getState();
+      expect(state.countries.length).to.equal(5);
+
+      let removeCountry = countriesSample[2];
+      action = actions.deleteCountry(removeCountry);
+      store.dispatch(action);
+      state = store.getState();
+      expect(state.countries.length).to.equal(4);
+
+      removeCountry = countriesSample[3];
+      action = actions.deleteCountry(removeCountry);
+      store.dispatch(action);
+      state = store.getState();
+      expect(state.countries.length).to.equal(3);
+
+      //  cannot remove the same country twice
+      removeCountry = countriesSample[3];
+      action = actions.deleteCountry(removeCountry);
+      store.dispatch(action);
+      state = store.getState();
+      expect(state.countries.length).to.equal(3);
     });
   });
 })
