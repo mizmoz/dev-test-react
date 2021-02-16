4b9270940495c6d931c4b2942b0d48ba2ae83fe9
delrosariotanny <tanny.delrosario@wondersco.com>
2021-02-16 18:56:28 -0500

    Set timeout back to 100

--- src/api/country.js
+++ src/api/country.js
@@ -7,5 +7,5 @@ import countries from '../configs/country';
  * @returns {Promise<any>}
  */
 export default () => new Promise((resolve, reject) => {
-  setTimeout(() => (Math.round(Math.random()) === 0 ? resolve(countries) : reject()), 1000);
+  setTimeout(() => (Math.round(Math.random()) === 0 ? resolve(countries) : reject()), 100);
 });
